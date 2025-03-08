0ad6f49b4ed5ec59f6b1e3e645cae61836bc97ad
“FelipeH” <felipehenriquedasilva@outlook.com>
2025-03-08 11:37:46 -0300

    front-end completed

--- musica.py
+++ musica.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect,session
+from flask import Flask, render_template, request, redirect,session, flash
 
 class Musica:
     def __init__(self, nome, artista, genero):
@@ -10,7 +10,7 @@ class Musica:
 
 musica01 = Musica('Temporal','Hungria','Rap')
 musica02 = Musica('A sua','Raimundos','Rock')
-musica03 = Musica('Vendaval','Fernandoe  Sorocaba','Sertanejo')
+musica03 = Musica('Vendaval','Fernando e  Sorocaba','Sertanejo')
 lista = [musica01,musica02,musica03]
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def listarMusicas():
 
     return render_template('lista_musicas.html',
                            musicas = lista,
-                           titulo = "Lista Musica"
+                           titulo = "Musicas cadastradas"
                            
                            )
 
@@ -35,7 +35,7 @@ def listarMusicas():
 def cadastrarMusicas():
     return render_template('cadastra_musica.html',
                            
-                           titulo = "Pagina de cadastro"
+                           titulo = "Cadastrar Musica"
                            
                            )
 
@@ -61,14 +61,16 @@ def Logar():
 def autenticar():
     if request.form['txtSenha'] == 'admin':
         session['usuario_logado'] = request.form['txtLogin']
+        flash("Usuario Logado com sucesso!")
         return redirect('/')
     else:
+        flash("Usuario/Senha invalida")
         return redirect("/login")
     
-app.route('/sair')
+@app.route('/sair')
 def sair():
     session['usuario_logado'] = None
-    return redirect('/')
+    return redirect('/login')
 
 
 app.run(debug=True)# debug = True evita ter que rodar manualmente o flask apos cada atualizacao no codigo
